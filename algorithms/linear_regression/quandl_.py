76e7eda337a1ddf36b5231046edc6a164311a42c
arslanhashmi <bsef14a522@pucit.edu.pk>
2017-10-21 14:05:04 +0500

    lib

--- algorithms/linear_regression/quandl_.py
+++ algorithms/linear_regression/quandl_.py
@@ -1,5 +1,46 @@
 import pandas as ps
 import quandl
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import pickle
+
+
 
 df = quandl.get('WIKI/GOOGL')
-print(df)
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High']-df['Adj. Low']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close']-df['Adj. Open']) / df['Adj. Open'] * 100
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume',]]
+#print (df.head())
+forecast_col = 'Adj. Close'
+df.fillna(-999999,inplace=True)
+forecast_out = int(math.ceil(0.01*len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+#print (df.tail())
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+X_train, X_test, y_train,  y_test = cross_validation.train_test_split(X, y, train_size=0.2)
+clf = LinearRegression(n_jobs=-1) #for maximum jobs -threads
+clf.fit(X_train,y_train)
+with open ('linearRegressionPickle.pickle','wb') as f:
+    pickle.dump(clf,f)
+
+pickle_in = open('linearRegressionPickle.pickle')
+clf = pickle.load(pickle_in)
+accuracy = clf.score(X_test, y_test)
+print (accuracy)
+
+
+
+
+
+
+
+
+
+
